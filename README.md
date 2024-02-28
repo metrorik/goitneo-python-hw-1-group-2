930b45a15dbb2f4da6b68fb28ed0382add1cacaa
metrorik <58214662+metrorik@users.noreply.github.com>
2024-02-28 21:37:31 +0100

    Update hw1_m8.py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Task 1.
+Task 1. Homework  GoIT Neoversity
 The function get_birthdays_per_week() to display a list of colleagues to be congratulated on their birthdays per week.
--- hw1_m8.py
+++ hw1_m8.py
@@ -9,21 +9,21 @@ def get_birthdays_per_week(users):
     curr_year = datetime.today().year
 
     for user in users:
-
+        
         usr_name = user["name"]
         birt_data = user["birthday"].date()
         birt_in_this_year = birt_data.replace(year=curr_year)
         birt_in_this_year_of_week = birt_in_this_year.strftime('%A')
 
-        if birt_in_this_year < curr_data:                               # д.р. в этом году уже был
+        if birt_in_this_year < curr_data:                               # there was already a birthday this year
             birt_in_this_year = birt_data.replace(year=curr_year+1)
         delta_days = (birt_in_this_year - curr_data).days
-        if delta_days < 7:                                              # д.р. на текущей неделе
-            if birt_in_this_year.weekday() < 5:                         # не попадает на выходные
+        if delta_days < 7:                                              # birthday will be this week
+            if birt_in_this_year.weekday() < 5:                         # birthday doesn't fall on the weekend
                 if birt_in_this_year_of_week not in birt_dict:
                     birt_dict[birt_in_this_year_of_week] = []
                 birt_dict[birt_in_this_year_of_week].append(usr_name)                   
-            else:                                                       # попадает на выходные, поздравить в понедельник
+            else:                                                       # falls on the weekend, congratulations on Monday
                 if "Monday" not in birt_dict:
                     birt_dict["Monday"] = []
                 birt_dict["Monday"].append(usr_name)
@@ -31,5 +31,4 @@ def get_birthdays_per_week(users):
     for day, names in sorted(birt_dict.items(), key=lambda x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].index(x[0])):
         formatted_output += f"{day}: {', '.join(names)}\n"
     
-    print(formatted_output)    
     return formatted_output
